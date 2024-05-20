9c87bbf88d1f7d21b8496dbd8cdae697c2f47813
Namitamunjal <namitamunjal27@gmail.com>
2024-05-20 10:41:19 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    extend: {
+      fontFamily: {
+        kons: ['Koh Santepheap', 'sans-serif'],
+        kelly:['Kelly Slab', 'sans-serif'],
+        alg:['Alegreya', 'serif'],
+        rb:['Roboto','serif']
+      },
+    },
+  },
+  plugins: [],
+}
+
